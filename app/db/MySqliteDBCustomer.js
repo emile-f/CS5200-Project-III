4e08831a67751cf0f33f6ad864121c728ad338bb
emile ferrand <emile@storied.co>
2021-10-31 19:15:18 -0700

    Add customer flow

--- app/db/MySqliteDBCustomer.js
+++ app/db/MySqliteDBCustomer.js
@@ -20,4 +20,152 @@ async function getCustomers() {
     }
 }
 
+async function insertCustomer(customer) {
+    const db = await open({
+        filename: "./db/database.db",
+        driver: sqlite3.Database,
+    });
+
+    const stmt = await db.prepare(`
+    INSERT INTO
+        Customer(name, smoker, drinkLevel, dressCodeID, ambience, budget)
+    VALUES (@name, @smoker, @drinkLevel, @dressCodeID, @ambience, @budget);
+    `);
+
+    const paymentStmt = await db.prepare(`
+    INSERT INTO
+        PaymentMethodsCustomer(customerId,paymentMethodsID)
+    VALUES (@customerId, @paymentMethodsID);
+    `);
+
+
+    const cuisineStmt = await db.prepare(`
+    INSERT INTO
+    CuisineCustomer(customerId,cuisineId)
+    VALUES (@customerId, @cuisineId);
+    `);
+
+    const query = {
+        "@name": customer.name,
+        "@smoker": customer.smoker,
+        "@drinkLevel": customer.drinkLevel,
+        "@dressCodeID": customer.dressCodeID,
+        "@ambience": customer.ambience,
+        "@budget": customer.budget
+    };
+
+    const paymentMethodQueries = [];
+    for (let index = 0; index < customer.paymentMethods.length; index++) {
+        paymentMethodQueries.push({
+            "@customerId": null,
+            "@paymentMethodsID": parseInt(customer.paymentMethods[index])
+        });
+    }
+
+    const cuisineQueries = [];
+    for (let index = 0; index < customer.cuisines.length; index++) {
+        cuisineQueries.push({
+            "@customerId": null,
+            "@cuisineId": parseInt(customer.cuisines[index])
+        });
+    }
+
+    try {
+        // insert customer
+        let returnId = await stmt.run(query);
+        if (returnId && returnId.lastID) {
+            returnId = returnId.lastID;
+        } else {
+            return
+        }
+
+        // prepare paymentMethods
+        for (let index = 0; index < paymentMethodQueries.length; index++) {
+            paymentMethodQueries[index]['@customerId'] = returnId;
+        }
+
+        // prepare cuisines
+        for (let index = 0; index < cuisineQueries.length; index++) {
+            cuisineQueries[index]['@customerId'] = returnId;
+        }
+
+        // insert paymentMethods
+        for (let index = 0; index < paymentMethodQueries.length; index++) {
+            await paymentStmt.run(paymentMethodQueries[index]);
+        }
+
+        for (let index = 0; index < cuisineQueries.length; index++) {
+            await cuisineStmt.run(cuisineQueries[index]);
+        }
+
+        return 'done'
+    } finally {
+        await stmt.finalize();
+        await paymentStmt.finalize();
+        await cuisineStmt.finalize();
+        db.close();
+    }
+}
+
+async function getCuisines() {
+    const db = await open({
+        filename: "./db/database.db",
+        driver: sqlite3.Database,
+    });
+
+    const stmt = await db.prepare(`
+    SELECT * FROM Cuisine
+    ORDER BY cuisine
+    `);
+
+    try {
+        return await stmt.all();
+    } finally {
+        await stmt.finalize();
+        db.close();
+    }
+}
+
+async function getPaymentMethods() {
+    const db = await open({
+        filename: "./db/database.db",
+        driver: sqlite3.Database,
+    });
+
+    const stmt = await db.prepare(`
+    SELECT * FROM PaymentMethods
+    ORDER BY method
+    `);
+
+    try {
+        return await stmt.all();
+    } finally {
+        await stmt.finalize();
+        db.close();
+    }
+}
+
+async function getDressCodes() {
+    const db = await open({
+        filename: "./db/database.db",
+        driver: sqlite3.Database,
+    });
+
+    const stmt = await db.prepare(`
+    SELECT * FROM DressCode
+    ORDER BY dressCode
+    `);
+
+    try {
+        return await stmt.all();
+    } finally {
+        await stmt.finalize();
+        db.close();
+    }
+}
+
 module.exports.getCustomers = getCustomers;
+module.exports.getCuisines = getCuisines;
+module.exports.getPaymentMethods = getPaymentMethods;
+module.exports.getDressCodes = getDressCodes;
+module.exports.insertCustomer = insertCustomer;
--- app/routes/customer.js
+++ app/routes/customer.js
@@ -9,4 +9,42 @@ router.get("/", async function (req, res, next) {
     res.render("customers", { customers });
 });
 
+router.get("/add", async function (req, res, next) {
+    const cuisines = await myDB.getCuisines();
+    const paymentMethods = await myDB.getPaymentMethods();
+    const dressCodes = await myDB.getDressCodes();
+    res.render('add-customer', { cuisines, paymentMethods, dressCodes });
+});
+
+router.post("/add", async function (req, res, next) {
+    console.log('test', req.body);
+
+    if (!req.body.name) {
+        next({ message: "Please provide a name for the customer" });
+        return
+    }
+
+    const customer = {
+        name: req.body.name,
+        smoker: req.body.smoker === 'smoker' ? 1 : 0,
+        drinkLevel: req.body.drinkLevel,
+        dressCodeID: parseInt(req.body.dressCode),
+        ambience: req.body.ambience,
+        budget: req.body.budget,
+        paymentMethods: req.body.paymentMethod,
+        cuisines: req.body.cuisine
+    };
+
+    console.log('customer', customer);
+
+    try {
+        const insertCustomer = await myDB.insertCustomer(customer);
+        console.log("Inserted", insertCustomer);
+        res.redirect('/customer');
+    } catch (err) {
+        console.log("Error inserting", err);
+        next(err);
+    }
+});
+
 module.exports = router;
