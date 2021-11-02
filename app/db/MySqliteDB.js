044c43313f92841a3e2c8b24c37e31b053004e4e
Misha-Mody <mishuu.mmm@gmail.com>
2021-11-02 10:48:25 -0700

    Create,Update,View Restaurants

--- app/db/MySqliteDB.js
+++ app/db/MySqliteDB.js
@@ -1,12 +1,233 @@
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 
-async function getRestaurants() {
-	const db = await open({
+async function connect() {
+	return await open({
 		filename: "./db/restaurant.db",
 		driver: sqlite3.Database,
 	});
-	return await db.all("SELECT * FROM Restaurant LIMIT 20");
 }
 
+async function getRestaurants(query, page, pageSize) {
+	const db = await connect();
+	const stmt = await db.prepare(`
+    SELECT * FROM Restaurant
+    WHERE name LIKE @query
+    LIMIT @pageSize
+    OFFSET @offset;
+    `);
+
+	const params = {
+		"@query": query + "%",
+		"@pageSize": pageSize,
+		"@offset": (page - 1) * pageSize,
+	};
+
+	try {
+		return await stmt.all(params);
+	} finally {
+		await stmt.finalize();
+		db.close();
+	}
+}
+
+async function getRestaurantCount(query) {
+	const db = await connect();
+	const stmt = await db.prepare(`
+    SELECT COUNT(*) AS count
+    FROM Restaurant
+    WHERE name LIKE @query;
+    `);
+
+	const params = {
+		"@query": query + "%",
+	};
+
+	try {
+		return (await stmt.get(params)).count;
+	} finally {
+		await stmt.finalize();
+		db.close();
+	}
+}
+
+async function viewRestaurantsyID(restID) {
+	const db = await connect();
+	const stmt = await db.prepare(`SELECT *
+    FROM Restaurant
+    WHERE
+      restID = :restID
+  `);
+
+	stmt.bind({
+		":restID": restID,
+	});
+
+	return await stmt.get();
+}
+
+async function createRestaurant(r) {
+	const db = await connect();
+
+	const stmt = await db.prepare(`INSERT INTO
+    Restaurant
+    VALUES (:restID,:name,:address,:zip,:city,:state,:country,:dressCodeID,:priceRangeMin,:priceRangeMax,:openHours,:closeHours)
+  `);
+
+	stmt.bind({
+		restID: r.restID,
+		":name": r.name,
+		":address": r.address,
+		":zip": r.zip,
+		":city": r.city,
+		":state": r.state,
+		":country": r.country,
+		":dressCodeID": r.dressCodeID,
+		":priceRangeMin": r.priceRangeMin,
+		":priceRangeMax": r.priceRangeMin,
+		":openHours": r.openHours,
+		":closeHours": r.closeHours,
+	});
+
+	return await stmt.run();
+}
+
+async function updateRestaurant(r) {
+	const db = await connect();
+	const stmt = await db.prepare(`UPDATE 
+    Restaurant SET
+    restID = :restID,
+    name=:name,
+    address=:address,
+    zip=:zip,
+    city=:city,
+    state=:state,
+    country=:country,
+    dressCodeID=:dressCodeID,
+    priceRangeMin=:priceRangeMin,
+    priceRangeMax=:priceRangeMax,
+    openHours=:openHours,
+    closeHours=:closeHours
+    WHERE
+    restID = :restID
+  `);
+	stmt.bind({
+		":restID": r.restID,
+		":name": r.name,
+		":address": r.address,
+		":zip": r.zip,
+		":city": r.city,
+		":state": r.state,
+		":country": r.country,
+		":dressCodeID": r.dressCodeID,
+		":priceRangeMin": r.priceRangeMin,
+		":priceRangeMax": r.priceRangeMax,
+		":openHours": r.openHours,
+		":closeHours": r.closeHours,
+	});
+	return await stmt.run();
+}
+
+async function viewServices(restID) {
+	const db = await connect();
+	const stmt = await db.prepare(`SELECT *
+    FROM Services
+    WHERE
+      restID = :restID
+  `);
+
+	stmt.bind({
+		":restID": restID,
+	});
+
+	return await stmt.get();
+}
+
+async function viewFacilities(restID) {
+	const db = await connect();
+	const stmt = await db.prepare(`SELECT *
+    FROM Facilities
+    WHERE
+      restID = :restID
+  `);
+
+	stmt.bind({
+		":restID": restID,
+	});
+
+	return await stmt.get();
+}
+
+async function viewPaymentMethod(restID) {
+	const db = await connect();
+	const stmt = await db.prepare(`select P.method from PaymentMethods P 
+inner join PaymentMethodsRestaurant R
+on P.paymentMethodsID = R.paymentMethodsID
+where 
+R.restID = :restID;
+  `);
+
+	stmt.bind({
+		":restID": restID,
+	});
+
+	return await stmt.all();
+}
+
+async function viewWorkingDays(restID) {
+	const db = await connect();
+	const stmt = await db.prepare(`select D.days from Days D
+inner join WorkingDays W
+on W.daysID = D.daysID
+where 
+W.restID = :restID;
+  `);
+
+	stmt.bind({
+		":restID": restID,
+	});
+
+	return await stmt.all();
+}
+
+async function getCuisineByID(restID) {
+	const db = await connect();
+	const stmt = await db.prepare(`select C.cuisine from Cuisine C
+inner join CuisineRestaurant R
+on C.cuisineID = R.cuisineID
+where 
+R.restID = :restID;
+  `);
+
+	stmt.bind({
+		":restID": restID,
+	});
+
+	return await stmt.all();
+}
+
+//async function deleteRestaurant(restID) {
+// 	const db = await connect();
+// 	const stmt = await db.prepare(`DELETE FROM
+// 		Restaurant
+//     WHERE restID = :restID
+//   `);
+
+// 	stmt.bind({
+// 		":restID": restID,
+// 	});
+
+// 	return await stmt.run();
+// }
+
 module.exports.getRestaurants = getRestaurants;
+module.exports.viewRestaurantsyID = viewRestaurantsyID;
+module.exports.createRestaurant = createRestaurant;
+module.exports.updateRestaurant = updateRestaurant;
+//module.exports.deleteRestaurant = deleteRestaurant;
+module.exports.viewWorkingDays = viewWorkingDays;
+module.exports.viewPaymentMethod = viewPaymentMethod;
+module.exports.viewFacilities = viewFacilities;
+module.exports.viewServices = viewServices;
+module.exports.getRestaurantCount = getRestaurantCount;
+module.exports.getCuisineByID = getCuisineByID;
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,9 +4,111 @@ let router = express.Router();
 const myDB = require("../db/mySqliteDB.js");
 
 /* GET home page. */
+/* GET home page. */
 router.get("/", async function (req, res, next) {
-  const restaurant = await myDB.getRestaurants();
-  res.render("index", { restaurants: restaurant });
+  res.redirect("/restaurants");
+});
+
+router.get("/restaurants", async function (req, res, next) {
+  const query = req.query.q || "";
+  const page = +req.query.page || 1;
+  const pageSize = +req.query.pageSize || 24;
+  try {
+    let total = await myDB.getRestaurantCount(query);
+    let restaurant = await myDB.getRestaurants(query, page, pageSize);
+    res.render("index2", {
+      restaurants: restaurant,
+      query,
+      currentPage: page,
+      lastPage: Math.ceil(total / pageSize),
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//router.post("/restaurants/delete", async function (req, res, next) {
+//   const id = req.body;
+//   console.log(id);
+//   await myDB.deleteRestaurant(id);
+//   res.redirect("/restaurants");
+// });
+
+router.get("/restaurants/add", async function (req, res, next) {
+  res.render("addRestaurant", {});
+});
+
+router.get("/restaurants/cuisine", async function (req, res, next) {
+  res.render("cuisine.ejs", {});
+});
+
+router.get("/restaurants/:restID", async function (req, res, next) {
+  const restID = req.params.restID;
+  const restaurant = await myDB.viewRestaurantsyID(restID);
+  const facilities = await myDB.viewFacilities(restID);
+  const services = await myDB.viewServices(restID);
+  const payment = await myDB.viewPaymentMethod(restID);
+  const working = await myDB.viewWorkingDays(restID);
+  const cuisine = await myDB.getCuisineByID(restID);
+  const days = [];
+  working.forEach((r) => {
+    days.push(r.days);
+  });
+
+  const pays = [];
+  payment.forEach((r) => {
+    pays.push(r.method);
+  });
+  res.render("restaurantByID", {
+    r: restaurant,
+    f: facilities,
+    s: services,
+    p: pays,
+    w: days,
+    c: cuisine,
+  });
+});
+
+router.get("/restaurants/view/:restID", async function (req, res, next) {
+  const restID = req.params.restID;
+  const restaurant = await myDB.viewRestaurantsyID(restID);
+  const facilities = await myDB.viewFacilities(restID);
+  const services = await myDB.viewServices(restID);
+  const payment = await myDB.viewPaymentMethod(restID);
+  const working = await myDB.viewWorkingDays(restID);
+  const cuisine = await myDB.getCuisineByID(restID);
+  const days = [];
+  working.forEach((r) => {
+    days.push(r.days);
+  });
+  console.log(cuisine);
+  const pays = [];
+  payment.forEach((r) => {
+    pays.push(r.method);
+  });
+  res.render("viewDetails", {
+    r: restaurant,
+    f: facilities,
+    s: services,
+    p: pays,
+    w: days,
+    c: cuisine,
+  });
+});
+
+router.post("/restaurants/create", async function (req, res, next) {
+  const rest = req.body;
+  await myDB.createRestaurant(rest);
+  res.redirect("/restaurants");
+});
+
+router.post("/restaurants/update/:restID", async function (req, res, next) {
+  const rest = req.body;
+  console.log(rest);
+  await myDB.updateRestaurant(rest);
+  //res.redirect("/restaurants");
+
+  res.render("restaurantByID", { r: rest });
 });
 
 module.exports = router;
