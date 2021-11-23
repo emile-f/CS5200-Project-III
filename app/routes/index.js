b23d42fc0ef7e3bacec6d8f406af4a23815e3d87
Misha-Mody <mishamody7@gmail.com>
2021-11-23 14:54:15 -0800

    Convert SQL TO MQL Queries

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,8 @@ let router = express.Router();
 // Routes
 const customerRoute = require("./customer");
 const ratingRoute = require("./rating");
-const myDB = require("../db/MySqliteDB");
+const myDB = require("../db/MyMongoDB");
+
 
 /* GET home page. */
 router.get("/", async function (req, res) {
@@ -15,14 +16,14 @@ router.use("/customer", customerRoute);
 router.use("/rating", ratingRoute);
 
 router.get("/restaurants", async function (req, res, next) {
-  const zip = req.query.z || "";
+  const zip = req.query.z || null;
   const query = req.query.q || "";
   const page = +req.query.page || 1;
   const pageSize = +req.query.pageSize || 24;
   const filter = req.query.filter;
   console.log(filter);
   try {
-    let total = await myDB.getRestaurantCount(query);
+    let total = await myDB.getRestaurantCount(query,zip);
     let restaurant = await myDB.getRestaurants(zip, query, page, pageSize);
     res.render("index2", {
       restaurants: restaurant,
@@ -68,55 +69,44 @@ router.get("/restaurants/cuisine/:cuisine", async function (req, res) {
 
 router.get("/restaurants/:restID", async function (req, res) {
   const restID = req.params.restID;
-  const restaurant = await myDB.viewRestaurantsByID(restID);
-  const facilities = await myDB.viewFacilities(restID);
-  const services = await myDB.viewServices(restID);
-  const payment = await myDB.viewPaymentMethod(restID);
-  const working = await myDB.viewWorkingDays(restID);
-  const cuisine = await myDB.getCuisineByID(restID);
-  const days = [];
-  working.forEach((r) => {
-    days.push(r.days);
-  });
+  const restaurantArr = await myDB.viewRestaurantsByID(restID);
+  const restaurant = restaurantArr[0];
 
-  const pays = [];
-  payment.forEach((r) => {
-    pays.push(r.method);
-  });
   res.render("restaurantByID", {
     r: restaurant,
-    f: facilities,
-    s: services,
-    p: pays,
-    w: days,
-    c: cuisine,
+    f: restaurant.facilities,
+    s: restaurant.services,
+    p: restaurant.payments,
+    w: restaurant.workingDays,
+    c: restaurant.cuisine,
   });
 });
 
 router.get("/restaurants/view/:restID", async function (req, res) {
   const restID = req.params.restID;
-  const restaurant = await myDB.viewRestaurantsByID(restID);
-  const facilities = await myDB.viewFacilities(restID);
-  const services = await myDB.viewServices(restID);
-  const payment = await myDB.viewPaymentMethod(restID);
-  const working = await myDB.viewWorkingDays(restID);
-  const cuisine = await myDB.getCuisineByID(restID);
-  const days = [];
-  working.forEach((r) => {
-    days.push(r.days);
-  });
-  console.log(cuisine);
-  const pays = [];
-  payment.forEach((r) => {
-    pays.push(r.method);
-  });
+  const restaurantArr = await myDB.viewRestaurantsByID(restID);
+  const restaurant = restaurantArr[0];
+  // const facilities = await myDB.viewFacilities(restID);
+  // const services = await myDB.viewServices(restID);
+  // const payment = await myDB.viewPaymentMethod(restID);
+  // const working = await myDB.viewWorkingDays(restID);
+  // const cuisine = await myDB.getCuisineByID(restID);
+  // const days = [];
+  // working.forEach((r) => {
+  //   days.push(r.days);
+  // });
+  // console.log(cuisine);
+  // const pays = [];
+  // payment.forEach((r) => {
+  //   pays.push(r.method);
+  // });
   res.render("viewDetails", {
     r: restaurant,
-    f: facilities,
-    s: services,
-    p: pays,
-    w: days,
-    c: cuisine,
+    f: restaurant.facilities,
+    s: restaurant.services,
+    p: restaurant.payments,
+    w: restaurant.workingDays,
+    c: restaurant.cuisine,
   });
 });
 
@@ -126,13 +116,22 @@ router.post("/restaurants/create", async function (req, res) {
   res.redirect("/restaurants");
 });
 
-router.post("/restaurants/update/:restID", async function (req, res) {
+router.post("/restaurants/update/:restID", async function (req,res) {
   const rest = req.body;
-  console.log(rest);
+  //console.log(rest);
   await myDB.updateRestaurant(rest);
-  res.redirect("/restaurants");
+  //res.redirect("/restaurants");
+  const restaurantArr = await myDB.viewRestaurantsByID(rest.restID);
+  const restaurant = restaurantArr[0];
 
-  //res.render("restaurantByID", { r: rest });
+  res.render("restaurantByID", {
+    r: restaurant,
+    f: restaurant.facilities,
+    s: restaurant.services,
+    p: restaurant.payments,
+    w: restaurant.workingDays,
+    c: restaurant.cuisine,
+  });
 });
 
 module.exports = router;
